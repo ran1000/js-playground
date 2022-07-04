c34736da1b1b1a94ea75fd937b145cf1a229192c
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2022-07-04 11:12:23 +0200

    add animation

--- src/index.js
+++ src/index.js
@@ -24,7 +24,27 @@ domRoot.style.color = "red";
 welcImg[3].src = "https://images.unsplash.com/photo-1568749229204-d0bae9465fdb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80";
 welcImg[3].style.height = "300px";
 // Adding and Removing
-
+const newText = document.createTextNode("A description for an not yet existing tag");
+console.log(newText);
+const subTitle = document.createElement("h3");
+subTitle.appendChild(newText);
+domRoot.appendChild(subTitle);
+const textAfterTag = document.createTextNode("Now a description for an existing tag");
+subTitle.replaceChild(textAfterTag, newText);
+// Animations
+let pos = 0;
+const t = setInterval(move, 1);
+domRoot.style.position = "relative";
+welcImg[3].style.position = "absolute";
+function move() {
+  if (pos >= window.innerWidth) {
+    pos = 0;
+  } else {
+    pos += 1;
+    welcImg[3].style.left = `${pos}px`;
+  }
+}
 // Event Handler
+
 
 // Event Listener
