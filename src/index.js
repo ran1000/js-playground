3d6dda0c82e52325ae65751a0b0415dc1866174c
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2022-07-04 10:28:29 +0200

    add selectors and changing scripts

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-alert('Welcome to Webpack Boilerplate');
-const user = prompt("What's your name?");
+// alert('Welcome to Webpack Boilerplate');
+// const user = prompt("What's your name?");
 
-class Greeter {
-  constructor(name) {
-    this.name = name;
-  }
+// class Greeter {
+//   constructor(name) {
+//     this.name = name;
+//   }
 
-  sayHello() {
-    document.body.innerHTML = `Hello ${this.name}! Your Boilerplate is properly running.`;
-  }
-}
+//   sayHello() {
+//     document.body.innerHTML = `Hello ${this.name}! Your Boilerplate is properly running.`;
+//   }
+// }
 
-const greeter = new Greeter(user);
-greeter.sayHello();
+// const greeter = new Greeter(user);
+// greeter.sayHello();
+
+// Selectors
+const domRoot = document.getElementById("root");
+const welcImg = domRoot.childNodes;
+console.log(domRoot);
+console.log(welcImg);
+// Changing Element
+domRoot.style.color = "red";
+welcImg[3].src = "https://images.unsplash.com/photo-1568749229204-d0bae9465fdb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80";
+welcImg[3].style.height = "300px";
+// Adding and Removing
+
+// Event Handler
+
+// Event Listener
